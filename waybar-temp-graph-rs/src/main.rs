1c1bd092afd65223d023a79d84e0ff73a43cae8c
Fernando Seoane <fseoane@hotmail.com>
2025-04-16 09:14:14 +0200

    Update character arrays in graph modules to use 'a' instead of '0' for consistency in display

--- waybar-temp-graph-rs/src/main.rs
+++ waybar-temp-graph-rs/src/main.rs
@@ -3,7 +3,7 @@ use std::env;
 use std::{thread, time::Duration};
 
 const COLORS:&[&str] = &["#96faf7","#66f1d7","#67f08d","#85f066","#f0ea66","#f0b166","#f09466","#f28888","#f37777","#f85555"];
-const CHARS: &[&str]=  &["0","b","c","d","e","f","g","h","i","j"];
+const CHARS: &[&str]=  &["a","b","c","d","e","f","g","h","i","j"];
 //const CHARS: &[&str]=  &[" ","▁","▂","▃","▄","▅","▆","▇","█","█"];   // with mono nerd fonts
 
 fn display_help() {
