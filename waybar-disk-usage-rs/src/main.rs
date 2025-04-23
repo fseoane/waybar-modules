bc2d29b092d5d8902ad91ae1e75e2aec2a1d2dbd
Fernando Seoane <fseoane@hotmail.com>
2025-04-23 15:07:24 +0200

    Fix color definition for disk usage display

--- waybar-disk-usage-rs/src/main.rs
+++ waybar-disk-usage-rs/src/main.rs
@@ -5,7 +5,7 @@ use std::{thread, time::Duration};
 //const COLORSUP:&[&str] =  &["#f299b9","#f288a9","#f29988","#f38877","#f37777","#f36677","#f35577","#f35566","#f74433","#f70011"];
 // const COLORSDOWN:&[&str] =&["#97f0cd","#87f0bd","#77f0ad","#87f0ad","#67f09d","#47f08d","#37f08d","#27f08d","#17f08d","#07f08d"];
 const COLORSUP:&[&str] =&["##53EDE8","#53E3ED","#53CCED","#53BAED","#53A8ED","#539BED","#538BED","#536FED","#535BED","#6553ED"];
-const COLORSDOWN:&[&str] =&["##E7BBF0","#F0B6F2","#F099ED","#F07FD9","#EB71BE","#EB609E","#ED5C92","#ED5379","#ED405A","##ED0C10"];
+const COLORSDOWN:&[&str] =&["#E7BBF0","#F0B6F2","#F099ED","#F07FD9","#EB71BE","#EB609E","#ED5C92","#ED5379","#ED405A","##ED0C10"];
 const CHARSUP: &[&str]=   &["0","b","c","d","e","f","g","h","i","j"];       // font efe-graph.ttf
 const CHARSDOWN: &[&str]= &["k","l","m","n","o","p","q","r","s","t"];       // font efe-graph.ttf
 
