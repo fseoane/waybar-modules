ee05724ca36b25c286d5c5f23258884aef2e5fbf
Fernando Seoane <fseoane@hotmail.com>
2025-04-23 15:14:29 +0200

    Update limits for disk usage statistics to include additional thresholds

--- waybar-disk-usage-rs/src/main.rs
+++ waybar-disk-usage-rs/src/main.rs
@@ -136,7 +136,7 @@ fn main() {
         if max_write_stats > highest{
             highest = max_write_stats;
         }
-        let limits = vec![5,15,60,150,1500,3000,6000,10000,20000];
+        let limits = vec![5,15,30,50,75,100,150,1000,2000,3000,6000,10000,20000];
         let mut max = 0;
         for limit in limits{
             if highest % limit == highest {
